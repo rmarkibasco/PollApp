e190146e8f5aab4b9d5898ea6100c8c8f22bb612
Mark Ibasco <rmarkibasco@gmail.com>
2015-01-19 20:14:18 +0800

    Django Tutorials Second page

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from polls.models import Choice, Poll
 
-# Register your models here.
+class ChoiceInline(admin.TabularInline):
+	model = Choice
+	extra = 3
+
+class PollAdmin(admin.ModelAdmin):
+	list_display = ('question', 'pub_date', 'was_published_recently')
+	#filter on the side of the form
+	list_filter = ['pub_date']
+	#search
+	search_fields = ['question']
+
+	fieldsets = [
+		(None,               {'fields': ['question']}),
+		('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+	]
+	inlines = [ChoiceInline]
+
+admin.site.register(Poll, PollAdmin)
--- polls/models.py
+++ polls/models.py
@@ -15,6 +15,11 @@ class Poll(models.Model):
 	def was_published_recently(self):
 		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
+	#(page 3)
+	was_published_recently.admin_order_field = 'pub_date'
+	was_published_recently.boolean = True
+	was_published_recently.short_description = 'Published Recently?'
+
 class Choice(models.Model):
 	poll = models.ForeignKey(Poll)
 	choice_text = models.CharField(max_length=200)
